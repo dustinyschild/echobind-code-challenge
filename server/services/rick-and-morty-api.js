464be6a0ff1cc47e0d2a59fe6e0fab277f6b4460
Dustin Yelland <dyelland@gmail.com>
2022-05-15 18:09:39 -0500

    add data fetching for the other resources for pages

--- server/services/rick-and-morty-api.js
+++ server/services/rick-and-morty-api.js
@@ -1,13 +1,29 @@
-const apiConfig = {
+const api = {
   url: "https://rickandmortyapi.com/api"
 };
 
 export const getAllLocations = async () => {
-  const res = await fetch(`${apiConfig.url}/location`);
+  const res = await fetch(`${api.url}/location`);
 
   const data = await res.json();
 
   // some error handling or validation here?
+
+  return data;
+};
+
+export const getLocation = async (id) => {
+  const res = await fetch(`${api.url}/location/${id}`);
+
+  const data = await res.json();
+
+  return data;
+};
+
+export const getCharacter = async (id) => {
+  const res = await fetch(`${api.url}/character/${id}`);
+
+  const data = await res.json();
 
   return data;
 };
