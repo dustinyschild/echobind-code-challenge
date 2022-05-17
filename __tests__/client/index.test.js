e56d9a5dece065d0b0a91d5ec7c3d4752dc58b11
Dustin Yelland <dyelland@gmail.com>
2022-05-17 02:53:36 -0500

    add props to test

--- __tests__/client/index.test.js
+++ __tests__/client/index.test.js
@@ -4,7 +4,21 @@ import { withTheme } from "../../helpers/withTheme";
 
 describe("Home", () => {
   it("renders a heading", () => {
-    const { getByText } = RTL.render(withTheme(<Home />));
+    const props = {
+      locations: [
+        {
+          id: 1,
+          name: "Earth",
+          type: "Planet",
+          dimension: "C-137",
+          residents: [],
+          created: "datetime string",
+          url: "#"
+        }
+      ]
+    };
+
+    const { getByText } = RTL.render(withTheme(<Home {...props} />));
 
     const heading = getByText("Keeping Up with the Cronenbergs");
 
